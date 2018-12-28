5e51723e73237c879537c3e23543500e1056c94f
Gene <g.larose@frontier.com>
2018-12-27 23:20:11 -0500

    added dashboard, forum and blog routes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app id="inspire" dark>
     <v-navigation-drawer v-model="drawer" clipped fixed app>
       <v-list dense>
-        <v-list-tile @click>
+        <v-list-tile router to="/">
           <v-list-tile-action>
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
@@ -10,19 +10,27 @@
             <v-list-tile-title>Dashboard</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click>
+        <v-list-tile router to="/forum">
           <v-list-tile-action>
-            <v-icon>settings</v-icon>
+            <v-icon>question_answer</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
+            <v-list-tile-title>Forum</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile router to="/blog">
+          <v-list-tile-action>
+            <v-icon>bookmarks</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Blog</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Code Community</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
